c702ca871de474937c02dcc43029b98018eba327
mkokkerametla <96996152+mkokkerametla@users.noreply.github.com>
2022-02-26 12:40:03 -0600

    Add Swagger UI

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-app.MapGet("/", () => "Hello World!");
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/hello", () => "Hello World!");
 
 app.Run();
